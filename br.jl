9e057788f0c21e8fb22a49e666fe51e1651e1e06
Roberto Esposito <roberto.esposito@unito.it>
2016-10-20 18:39:21 +0200

    New version with fix! operator

--- br.jl
+++ br.jl
@@ -1,6 +1,7 @@
 using Convex
 using DataFrames
 using Gadfly
+using Logging
 
 data = readtable("LogPTol_vsPlusDescr.csv", separator=';')
 blocks = readtable("LogPTol_vsPlusDescr_blocks.csv", separator=';')
@@ -32,24 +33,27 @@ previous_optval = 100
 i=0
 maxiterations = 5
 
+# prediction =  X * (P .* α) * β
+# the current form is a workaround for a bug in Convex.jl .* implementation
+prediction =  X * (P .* (α * ones(1,K))) * β
+problem = minimize( norm(prediction - y) )
+
+Logging.configure(level=INFO)
+
 while δ > ϵ && i < maxiterations
-  # α problem: solve for best αs keeping β constant
-  b = β.value
-  # the following shuold be
-  # prediction_α =  X * (P .* α) * b
-  # the current form is a workaround for a bug in Convex.jl .* implementation
-  prediction_α =  X * (P .* (α * ones(1,K))) * b
-  problem_α = minimize( norm(prediction_α - y) )
-  solve!(problem_α)
+  info("Starting iteration n. $i")
+  # First solve fix beta and solve for alpha
+  fix!(β)
+  solve!(problem)
+  free!(β)
 
-  # β problem: solve for best βs keeping α constant
-  a = α.value
-  prediction_β = X * (P .* a) * β
-  problem_β = minimize( norm(prediction_β - y) )
-  solve!(problem_β)
+  # Then fix alpha and solve for beta
+  fix!(α)
+  solve!(problem)
+  free!(α)
 
-  δ = abs(previous_optval - problem_β.optval)
-  previous_optval = problem_β.optval
+  δ = abs(previous_optval - problem.optval)
+  previous_optval = problem.optval
   i+=1
 
   println("δ: $δ")
@@ -57,20 +61,21 @@ while δ > ϵ && i < maxiterations
   println("β: $(β.value)")
 end
 
+println("Stopped after $i iterations")
 println("α: $(α.value)")
 println("β: $(β.value)")
 
-a = α.value
-b = β.value
-loss = norm( X * (P .* a) * b - y)
+loss = eval(norm( X * (P .* (α * ones(1,K)) * β) - y))
 
 println("loss: $loss")
 
+a= α.value
+b = β.value
 
 #println(barplot(convert(Array, blocks[:Descriptor]), abs(round(a[:,1],6)), title = "α"))
 α_plot = plot( x=blocks[:Descriptor], y=round(a[:,1],6), Geom.bar, Theme(minor_label_font_size=3pt) )
-draw( PNG("alpha_plot.png"), α_plot)
+# draw( PNG("alpha_plot.png"), α_plot)
 
 #println(barplot(names(blocks)[2:7], abs(b[:,1]), title = "β"))
 β_plot = plot( x=names(blocks)[2:7], y=b[:,1], Geom.bar )
-draw( PNG("beta_plot.png"), β_plot)
+# draw( PNG("beta_plot.png"), β_plot)
