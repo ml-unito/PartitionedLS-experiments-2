deaede6c1ff0caa08389e3a51decfc942b545801
Roberto Esposito <roberto.esposito@unito.it>
2016-10-21 17:44:51 +0200

    Further refactoring.

--- TLLR.jl
+++ TLLR.jl
@@ -3,58 +3,104 @@ module TLLR
 using Convex
 using Logging
 
-export TLLRegression, fit, predict
+import Base.size
+export TLLRegression, fit, predict, alpha, beta, optval
 
-# Two Levels Linear Regression
+"""
+    type TLLRegression
+
+The type of Two Levels Linear Regression models.
+
+A TLLR model is a linear model has the form:
+  \$h(x) = \sum_k \beta_k f_{k}(x)\$
+
+where \$f_\beta(x)\$ are pseudo features built on the original \$x\$
+features:
+  \$f_{\beta_k}(x) = \sum_{m: P_{m,k} = 1} \alpha_m x_m\$
+"""
 type TLLRegression
   α::Variable
   β::Variable
   P::Array{Int,2}
   model::AbstractExpr
+  problem::Problem
+
+  function TLLRegression(P)
+    M, K = size(P)
+    tll = new()
+    tll.α = Variable(M, Positive())
+    tll.β = Variable(K)
+    tll.P = P
+    tll.model = (tll.P .* (tll.α * ones(1,size(tll.P,2)))) * tll.β
+
+    tll.β.value = rand(K)
+    tll
+  end
 end
 
-TLLRegression(α,β,P) = TLLRegression(α,β,P,(P .* (α * ones(1,size(P,2)))) * β)
+# tll.α.value is a matrix,
+function alpha(tll::TLLRegression)
+  tll.α.value[:,1]
+end
 
+function beta(tll::TLLRegression)
+  tll.β.value[:,1]
+end
+
+function size(tll::TLLRegression)
+  size(tll.P)
+end
+
+optval(tll::TLLRegression) = tll.problem.optval
+
+function solve_for(tll::TLLRegression, var::Symbol)
+  fix!(getfield(tll,var))
+  solve!(tll.problem)
+  free!(getfield(tll, var))
+end
+
+"""
+    fit(X::Array{Float64,2}, y::Array{Float64,1}, P::Array{Int,2})
+
+Fits a TLLRegression model to the given data and resturns the
+learnt model (`::TLLRegression`).
+
+# Arguments
+
+* `X`: NxM matrix describing the examples
+* `y`: N vector with the output values for each examples
+* `P`: MxK matrix specifying how to partition the M attributes into
+    K subsets. P(m,k) should be 1 if attribute number m belongs to
+    partition k.
+
+"""
 function fit(X::Array{Float64,2}, y::Array{Float64,1}, P::Array{Int,2})
   # row normalization
   X = X ./ sum(X,2)
 
-  M = size(X,2)
-  K = size(P,2)
-
-  tll = TLLRegression(Variable(M, Positive()), Variable(K), P)
-
-  # initialization of β to a random vector
-  tll.β.value = rand(K)
+  tll = TLLRegression(P)
+  M,K = size(tll)
 
   # termination criteria is guided by epsilon... when two iterations optvalue
-  # differ for a quantity that is less than epslon, we stop
-  ϵ = 0.01
+  # differ for a quantity δ that is less than epslon, we stop
+  ϵ = 0.001
   δ = 1.0
 
   previous_optval = 100
-
   i=0
   maxiterations = 5
 
-  problem = minimize( norm(X * tll.model - y) )
+  tll.problem = minimize( norm(X * tll.model - y) )
 
   Logging.configure(level=INFO)
 
   while δ > ϵ && i < maxiterations
     info("Starting iteration n. $i")
-    # First solve fix beta and solve for alpha
-    fix!(tll.β)
-    solve!(problem)
-    free!(tll.β)
+    solve_for(tll, :β)
+    solve_for(tll, :α)
 
-    # Then fix alpha and solve for beta
-    fix!(tll.α)
-    solve!(problem)
-    free!(tll.α)
-
-    δ = abs(previous_optval - problem.optval)
-    previous_optval = problem.optval
+    δ = abs(previous_optval - optval(tll))
+    previous_optval = optval(tll)
     i+=1
   end
 
--- br.jl
+++ br.jl
@@ -1,8 +1,9 @@
-push!(LOAD_PATH, ".")
+include("./TLLR.jl")
+
 
 using DataFrames
 using Gadfly
-using TLLR: fit, predict
+using TLLR: fit, predict, alpha, beta, optval
 
 
 # main
@@ -13,19 +14,19 @@ blocks = readtable("LogPTol_vsPlusDescr_blocks.csv", separator=';')
 
 X = convert(Array, data[:, 2:83])
 y = convert(Array, data[:, :log_Ptol])
-P = convert(Array, blocks[:, 2:7])   # should be a matrix of M x K bits, P[m,k] = 1 if feature m is in P[k], 0 otherwise
+P = convert(Array, blocks[:, 2:7])
 
 tll = fit(X, y, P)
 
-a = tll.α.value
-b = tll.β.value
+α = alpha(tll)
+β = beta(tll)
 
 println( "loss: $(norm(predict(tll, X)-y))")
 
 #println(barplot(convert(Array, blocks[:Descriptor]), abs(round(a[:,1],6)), title = "α"))
-α_plot = plot( x=blocks[:Descriptor], y=round(a[:,1],6), Geom.bar, Theme(minor_label_font_size=3pt) )
+α_plot = plot( x=blocks[:Descriptor], y=round(α,6), Geom.bar, Theme(minor_label_font_size=3pt) )
 # draw( PNG("alpha_plot.png"), α_plot)
 
 #println(barplot(names(blocks)[2:7], abs(b[:,1]), title = "β"))
-β_plot = plot( x=names(blocks)[2:7], y=b[:,1], Geom.bar )
+β_plot = plot( x=names(blocks)[2:7], y=β, Geom.bar )
 # draw( PNG("beta_plot.png"), β_plot)
