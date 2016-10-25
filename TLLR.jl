9642faa6b96ccd98776bc9b3a2265d4262775aa4
Roberto Esposito <roberto.esposito@unito.it>
2016-10-25 15:32:37 +0200

    Version with workaround for the fix! bug

--- TLLR.jl
+++ TLLR.jl
@@ -2,6 +2,7 @@ module TLLR
 
 using Convex
 using Logging
+using SCS
 
 import Base.size
 export TLLRegression, fit, predict, alpha, beta, optval
@@ -24,14 +25,18 @@ type TLLRegression
   P::Array{Int,2}
   model::AbstractExpr
   problem::Problem
+  verbose::Int
+  X
+  y
 
-  function TLLRegression(P)
+  function TLLRegression(P; verbose=0)
     M, K = size(P)
     tll = new()
-    tll.α = Variable(M, Positive())
+    tll.α = Variable(M)
     tll.β = Variable(K)
     tll.P = P
     tll.model = (tll.P .* (tll.α * ones(1,size(tll.P,2)))) * tll.β
+    tll.verbose = verbose
 
     tll
   end
@@ -52,16 +57,43 @@ end
 
 optval(tll::TLLRegression) = tll.problem.optval
 
-function solve_for(tll::TLLRegression, var::Symbol)
-  info("solving for $(var)")
-  info("before learning $(var): $(getfield(tll,var).value)")
+function tllsolve!(tll::TLLRegression, freevarname; verbose=0)
+  fixvar_symbol = setdiff([:α,:β],[freevarname])[1]
+  fixvar = getfield(tll, fixvar_symbol)
+  freevar = getfield(tll, freevarname)
 
-  fix!(getfield(tll,var))
-  solve!(tll.problem)
-  free!(getfield(tll, var))
+  info("solving for $freevarname")
+  info("before learning $freevarname: $(freevar.value)")
+  info("$freevarname is free: $(!(:fixed in freevar.sets))")
 
-  info("after learning (status: $(tll.problem.status))$(var): $(getfield(tll,var).value)")
+  fix!(fixvar)
+  Convex.solve!(tll.problem, ECOSSolver(verbose=verbose))
+  free!(fixvar)
+
+  info("after learning (status: $(tll.problem.status)) $(freevarname): $(freevar)")
 end
+
+function tllsolvefor_alpha!(tll::TLLRegression; verbose=0)
+  tll.model = (tll.P .* (tll.α.value * ones(1,size(tll.P,2)))) * tll.β
+  tll.problem = minimize( norm(tll.X * tll.model - tll.y) )
+  Convex.solve!(tll.problem, SCSSolver(verbose=verbose))
+end
+
+function tllsolvefor_beta!(tll::TLLRegression; verbose=0)
+  tll.model = (tll.P .* (tll.α * ones(1,size(tll.P,2)))) * tll.β.value
+  tll.problem = minimize( norm(tll.X * tll.model - tll.y) )
+  Convex.solve!(tll.problem, SCSSolver(verbose=verbose))
+end
+
+
+function tllsolve2!(tll::TLLRegression, freevarname; verbose=0)
+  if(freevarname==:α)
+    tllsolvefor_alpha!(tll, verbose=verbose)
+  else
+    tllsolvefor_beta!(tll, verbose=verbose)
+  end
+end
+
 
 """
     fit(X::Array{Float64,2}, y::Array{Float64,1}, P::Array{Int,2}; beta=randomvalues)
@@ -79,17 +111,23 @@ learnt model (`::TLLRegression`).
 * `beta`: Initial value for betas
 
 """
-function fit(X::Array{Float64,2}, y::Array{Float64,1}, P::Array{Int,2}; beta = rand(size(P,2)))
+function fit(X::Array{Float64,2}, y::Array{Float64,1}, P::Array{Int,2}; beta = rand(size(P,2)), verbose=0)
 
-  Logging.configure(output=open("tllr.log", "a"), level=INFO)
+  # Logging.configure(output=open("tllr.log", "a"), level=INFO)
+  Logging.configure(level=INFO)
   info("------------ FIT ---------------")
 
   # row normalization
   X = X ./ sum(X,2)
 
-  tll = TLLRegression(P)
-  tll.β.value = beta
+  # FIXME: aggiungere termine noto
+
+  tll = TLLRegression(P, verbose=verbose)
+
   M,K = size(tll)
+
+  tll.α.value = rand(M)
+  tll.β.value = beta
 
   # termination criteria is guided by epsilon... when two iterations optvalue
   # differ for a quantity δ that is less than epslon, we stop
@@ -100,17 +138,23 @@ function fit(X::Array{Float64,2}, y::Array{Float64,1}, P::Array{Int,2}; beta = r
   i=0
   maxiterations = 5
 
+  # FIXME: The following two assignment do not belong here
+  tll.X = X
+  tll.y = y
   tll.problem = minimize( norm(X * tll.model - y) )
 
-  while δ > ϵ && i < maxiterations
+  while i < maxiterations
     info("Starting iteration n. $i")
-    solve_for(tll, :β)
-    solve_for(tll, :α)
+    tllsolve2!(tll, :α, verbose=verbose)
+    tllsolve2!(tll, :β, verbose=verbose)
 
     δ = abs(previous_optval - optval(tll))
     previous_optval = optval(tll)
+    info("Loss at iteration $i: $previous_optval")
     i+=1
   end
+
+  println( evaluate(X * tll.model - y) )
 
   return tll
 end
--- br.jl
+++ br.jl
@@ -2,7 +2,6 @@ include("./TLLR.jl")
 
 
 using DataFrames
-using Gadfly
 using TLLR: fit, predict, alpha, beta, optval
 
 
@@ -16,17 +15,22 @@ X = convert(Array, data[:, 2:83])
 y = convert(Array, data[:, :log_Ptol])
 P = convert(Array, blocks[:, 2:7])
 
-tll = fit(X, y, P, beta=[0.1,0.1,0.1,0.1,0.1,1.0])
+tll = fit(X, y, P, beta=[1,-1,1,-1,1,1], verbose=0)
 
 α = alpha(tll)
 β = beta(tll)
 
 println( "loss: $(norm(predict(tll, X)-y))")
+println("α: $α")
+println("β: $β")
 
-#println(barplot(convert(Array, blocks[:Descriptor]), abs(round(a[:,1],6)), title = "α"))
-α_plot = plot( x=blocks[:Descriptor], y=round(α,6), Geom.bar, Theme(minor_label_font_size=3pt) )
-draw( SVG("alpha_plot.svg", 14cm, 10cm), α_plot)
-#
-#println(barplot(names(blocks)[2:7], abs(b[:,1]), title = "β"))
-β_plot = plot( x=names(blocks)[2:7], y=β, Geom.bar )
-draw( SVG("beta_plot.svg", 14cm, 10cm), β_plot)
+
+if false
+  #println(barplot(convert(Array, blocks[:Descriptor]), abs(round(a[:,1],6)), title = "α"))
+  α_plot = plot( x=blocks[:Descriptor], y=round(α,6), Geom.bar, Theme(minor_label_font_size=3pt) )
+  draw( SVG("alpha_plot.svg", 14cm, 10cm), α_plot)
+  #
+  #println(barplot(names(blocks)[2:7], abs(b[:,1]), title = "β"))
+  β_plot = plot( x=names(blocks)[2:7], y=β, Geom.bar )
+  draw( SVG("beta_plot.svg", 14cm, 10cm), β_plot)
+end
