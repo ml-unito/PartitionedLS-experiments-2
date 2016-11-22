885e7c26bfbc51003c528c4e0a90cdfd1d04440a
Roberto Esposito <roberto.esposito@unito.it>
2016-11-22 12:20:29 +0100

    Improved documentation

--- TLLR.jl
+++ TLLR.jl
@@ -28,16 +28,29 @@ end
     fit(X::Array{Float64,2}, y::Array{Float64,1}, P::Array{Int,2}; beta=randomvalues)
 
 Fits a TLLRegression model to the given data and resturns the
-learnt model (`::TLLRegression`).
+learnt model (see the Result section).
 
 # Arguments
 
-* `X`: NxM matrix describing the examples
-* `y`: N vector with the output values for each examples
-* `P`: MxK matrix specifying how to partition the M attributes into
-    K subsets. P(m,k) should be 1 if attribute number m belongs to
-    partition k.
-* `beta`: Initial value for betas
+* `X`: \$N × M\$ matrix describing the examples
+* `y`: \$N\$ vector with the output values for each example
+* `P`: \$M × K\$ matrix specifying how to partition the \$M\$ attributes into
+    \$K\$ subsets. \$P_{m,k}\$ should be 1 if attribute number \$m\$ belongs to
+    partition \$k\$.
+* `verbose`: if true (or 1) the output of solver will be shown
+* `η`: regularization factor, higher values implies more regularized solutions
+
+# Result
+
+A tuple of the form: `(opt, a, b, t, P)`
+
+* `opt`: optimal value of the objective function (loss + regularization)
+* `a`: values of the α variables at the optimal point
+* `b`: values of the β variables at the optimial point
+* `t`: the intercept at the optimal point
+* `P`: the partition matrix (copied from the input)
+
+The output model predicts points using the formula: f(X) = \$X * (P .* a) * b + t\$.
 
 """
 function fit(X::Array{Float64,2}, y::Array{Float64,1}, P::Array{Int,2}; verbose=0, η=1)
@@ -76,6 +89,10 @@ end
   predict(model::Tuple, X::Array{Float64,2})
 
   returns the predictions of the given model on examples in X
+
+  #see
+
+    fit(X::Array{Float64,2}, y::Array{Float64,1}, P::Array{Int,2}; beta=randomvalues)
 """
 function predict(model, X::Array{Float64,2})
   (_, α, β, t, P) = model
