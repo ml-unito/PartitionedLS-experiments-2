b441b2e9cad0f68fc772646e369247b8b895fbbc
Roberto Esposito <roberto.esposito@unito.it>
2024-03-29 16:52:51 +0100

    feat: Add experiments execution script with specified algorithms and datasets
    Added a script to run experiments using specific algorithms on various datasets to evaluate model quality.

--- run_on_all_datasets.sh
+++
@@ -1,8 +0,0 @@
-
-
-julia $1 datasets/PartLSArtificial
-julia $1 datasets/Limpet
-julia $1 datasets/Facebook\ Comment\ Volume\ Dataset
-julia $1 datasets/Superconductivty\ Data
-julia $1 datasets/YearPredictionMSD
-
--- run_simple.sh
+++
@@ -1,8 +0,0 @@
-
-
-julia SimpleLeastSquares.jl $1
-julia SimplePartitionedLS.jl $1
-julia SimplePCLS.jl $1
-julia SimplePLS.jl $1
-
-julia CompareResults.jl
